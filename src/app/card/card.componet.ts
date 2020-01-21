58a5421838f51a7d97a3897669061c87107cf209
Demian Ivan <hagen_id@ukr.net>
2020-01-21 16:35:01 +0200

    First commit v1

---
+++ src/app/card/card.componet.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+import { templateSourceUrl } from '@angular/compiler';
+
+@Component ({
+    selector:'app-card',
+    templateUrl:'./card.component.html',
+    styleUrls: ['./card.component.scss']
+    
+   
+})
+export class CardComponent {}
